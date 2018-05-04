8a0e94269d6d8c7b381413e1a748c9e24d33c533
Nicholas Car <nicholas.car@csiro.au>
2018-05-04 10:12:32 +1000

    v1.4.0

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
-# PyProms
+# PyPROMS
 
-PyProms is a library of Python classes that helps Python coders generate provenance reports using the PROV and PROMS OWL Ontologies. It also has a function to send a Report to an instances of the [PROMS Server](http://promsns.org/wiki/proms) for storage.
+PyPROMS is a library of Python classes that helps Python coders generate provenance reports using the PROV and PROMS OWL Ontologies. It also has a function to send a Report to an instances of the [PROMS Server](http://promsns.org/wiki/proms) for storage.
 
-The classes match the PROMS-O OWL classes, for example the Python class 'Report' matches the PROMS-O 'Report' class.
+The Python classes match the PROMS-O OWL classes, for example the class 'Report' matches the PROMS-O 'Report' class.
 
 PyProms is expected to be extended so if you need so add to it to get your reporting job done, please do so! 
 
-### Contact
+## License
+PyPROMS is licensed using the MIT License, an a copy of the deed is contained within this repository:
 
-The development of PyProms is lead by Nicholas Car (<nicholas.car@ga.gov.au>) and includes members of the CSIRO informatics community. Please contact him for any help.
+* [License deed](LICENSE.txt)
+
+## Contact
+
+The development of PyProms is lead by Nicholas Car at the [CSIRO](http://csiro.au) and includes members of the CSIRO informatics community. Please contact him for any help.
+
+**Nicholas Car**
+*Senior Experimental Scientist*
+CSIRO Land & Water
+Brisbane, Queensland
+<nicholas.car@csiro.au>
+<https://orcid.org/0000-0002-8742-7730>
 
 We are interested in any extensions people have made to PyPROMS and are keen to take on suggestions so please get in touch!
--- setup.py
+++ setup.py
@@ -1,36 +1,46 @@
+#!/usr/bin/env python
 from setuptools import setup
+import io
+
+with io.open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name='pyproms',
     packages=['pyproms'],
-    version='1.2.0',
+    version='1.4.0',
+    description='PyPROMS is a client library to make provenance documents and submit them to a PROMS Server instance',
     author='Nicholas Car',
-    author_email='nicholas.car@ga.gov.au',
-    url='https://bitbucket.csiro.au/projects/EIS/repos/proms-clients-python',
-    download_url='https://bitbucket.csiro.au/rest/archive/latest/projects/EIS/repos/proms-clients-python/archive?format=zip',
+    author_email='nicholas.car@csiro.au',
+    url='https://github.com/CSIRO-enviro-informatics/pyproms',
+    download_url='https://github.com/CSIRO-enviro-informatics/pyproms/archive/master.zip',
     license='LICENSE.txt',
-    description='PyPROMS is a client library to make PROV and PROMS documents and submit them to a PROMS Server instance',
     keywords=['rdf', 'prov', 'prov-o', 'proms', 'proms-o', 'provenance'],
-    long_description='''
-PyProms is a library of classes that helps Python coders generate
-provenance reports using the PROV and PROMS Ontologies.
-
-The PROMS Ontology is at http://promsns.org/def/proms
-
-The Python classes match the PROMS-O OWL classes, for example the Python class
-'Report' matches the PROMS-O 'Report' class.
-
-PyProms is expected to be extended so if you need so add to it to get your
-reporting job done, please do so! If you're really nice, you might consider
-letting the author, Nicholas Car <nicholas.car@ga.gov.au> know what you've
-added as this will help with further releases.
-    ''',
-    install_requires=['rdflib >= 4.0.0'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Utilities',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/CSIRO-enviro-informatics/pyproms/issues',
+        'Source': 'https://github.com/CSIRO-enviro-informatics/pyproms/',
+    },
+    python_requires='>=3.0',
+    install_requires=[
+        'rdflib >= 4.0.0',
+        'requests'
     ],
 )
 
-#use http://peterdowns.com/posts/first-time-with-pypi.html
+ # use http://peterdowns.com/posts/first-time-with-pypi.html
